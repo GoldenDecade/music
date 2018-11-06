59fdc09d0a56255972b9c100453cf0ccf98c9286
wangqiang <wangqiangb@litsoft.com.cn>
2018-11-06 09:20:55 +0800

    init code

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+  //  首次加载完成移除动画
+    if(document.querySelector('#appLoading')) {
+      document.querySelector('#appLoading').classList.add('removeAnimate');
+      setTimeout(()=> {
+        document.body.removeChild(document.getElementById('appLoading'));
+        let version = getVersion(), newVersion = pkg.version;
+      }, 500)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
