6ba1e9c46db3689b4f1df7d0e3e4b56d03b059c2
wangqiang <wangqiangb@litsoft.com.cn>
2018-11-07 08:54:40 +0800

    console.info的使用

--- src/main.js
+++ src/main.js
@@ -9,6 +9,17 @@ import mmToast from 'base/mm-toast'
 Vue.use(mmToast);
 Vue.config.productionTip = false
 import '@/assets/css/index.styl'
+
+
+// 版权信息
+const pkg = require('../package.json');
+window.mmPlayer = window.mmplayer = `欢迎使用 mmPlayer!
+当前版本为：V${pkg.version}
+作者：GoldenDecade
+Github：https://github.com/GoldenDecade/music
+歌曲来源于网易云音乐 (http://music.163.com)`;
+console.info(`%c${mmPlayer}`, `color:blue`);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
